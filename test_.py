13a4aa323e0e8dfc98c9c04b57ac9f76f112c183
Atul Snapy <atulyadav7096@gmail.com>
2019-05-22 08:43:46 +0530

    test file added

---
+++ test_.py
@@ -0,0 +1,11 @@
+try:
+    import sys      # line-1
+    ब=10      # line-2
+    स=20      # line-3
+    print(ब==स)      # line-4
+    if      # line-5
+    if(      # line-6
+    print(sys.argv)      # line-7
+
+except Exception as e:
+    print(e)
