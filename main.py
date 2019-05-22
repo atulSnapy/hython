50d2dd118638c28f277aec412402eddddd3c7903
Atul Snapy <atulyadav7096@gmail.com>
2019-05-22 14:43:32 +0530

    id added into main.py

--- main.py
+++ main.py
@@ -28,12 +28,186 @@ def transpile(file_name):
     source_file.close()
 
 def get_ids():
-    ids  = []
-    file = open('id.csv', 'rb')
-
-    for line in file:
-        ids.append(tuple(line.decode('utf-8').split()))
-    file.close()
+    id_string = """
+        and	और
+        as	जैसे
+        assert	निश्चित
+        break	अवरोध
+        class	वर्ग
+        continue    जारी
+        def परि
+        del	मिटाओ
+        elif	या_अगर
+        else	अन्यथा
+        except	त्रुटि
+        False	असत्य
+        finally	अन्तिम
+        for	क्रमशः
+        global	सर्व
+        if	अगर
+        import	आयात
+        in	में
+        is	है
+        lambda	मिराज
+        None	शून्य
+        nonlocal	बेस्थानीय
+        not	नहीं
+        or	या
+        pass	छोडो
+        raise	उठाओ
+        return	वापस
+        True	सत्य
+        try	प्रयास
+        while	जबतक
+        with	लेकर
+        yield	उपज
+        . .
+        abs(	समपूर्ण(
+        all(	सब(
+        any(	कोईभी(
+        ascii(	आसकी(
+        bin(	द्विग(
+        bool(	द्वाफल(
+        bytearray(	बाइट_व्यूह(
+        bytes(	बाइटस(
+        callable(	प्रतिदेय(
+        chr(	अक्षर(
+        classmethod(	वर्ग_क्रिया(
+        compile(	संकलन(
+        complex(	जटिल(
+        delattr(	गुण_मिटाओ(
+        dict(	कोश(
+        dir(	संहिता(
+        divmod(	भाग_शेष(
+        enumerate(	क्रमागत(
+        eval(	मुल्यांकन(
+        exec(	प्रयोग(
+        filter(	छानो(
+        float(	चल(
+        format(	रचना(
+        frozenset(	अचल_सम्मुचय(
+        getattr(	गुण_लो(
+        globals(	सर्वकोश(
+        hasattr(	गुण_है(
+        hash(	हैश(
+        help(	मदद(
+        hex(	षष्ठ(
+        id(	पहचान(
+        input(	पूछो(
+        int(	अंक(
+        isinstance(	अवतार_है(
+        issubclass(	उपवर्ग_है(
+        iter(	पुनरावर्तक(
+        len(	माप(
+        list(	सुची(
+        locals(	देशकोश(
+        map(	नक्शा(
+        max(	उच्च(
+        memoryview(	स्मृति_दृश्य(
+        min(	न्यून(
+        next(	अगला(
+        object(	वस्तु(
+        oct(	अष्ट(
+        open(	खोलो(
+        ord(	यूनिकोड_अक्षर(
+        pow(	घात(
+        print(	लिखो(
+        property(	गुण(
+        range(	सीमा(
+        repr(	पठनीय(
+        reversed(	उलटा(
+        round(	पूूणांकन(
+        set(	समुच्चय(
+        setattr(	गुण_(
+        slice(	टुकडा(
+        sorted(	क्रमम्(
+        @staticmethod(	@अटल_क्रिया(
+        str(	माला(
+        sum(	योग(
+        tuple(	टपल(
+        type(	प्रकार(
+        vars(	चर(
+        zip(	संकु(
+        capitalize(	प्रथम_ऊ(
+        casefold(	प्रत्येक_नि(
+        center(	मध्य(
+        count(	गिनो(
+        encode(	कुटलेख(
+        endswith(	अंत_में(
+        expandtabs(	रिक्त_(
+        find(	खोजो(
+        format(	रचना(
+        format_map(	रचना_नक्शा(
+        index(	सूचक(
+        isalnum(	अक्षर_अंक_है(
+        isalpha(	अक्षर_है(
+        isdecimal(	दशमलव_है(
+        isdigit(	अंक_है(
+        isidentifier(	_है(
+        islower(	नीचला_है(
+        isnumeric(	अंक_है(
+        isprintable(	छाप_है(
+        isspace(	खाली_है(
+        istitle(	शीर्षक_है(
+        isupper(	ऊपरी_है(
+        join(	मेल(
+        ljust(	बाया_ज(
+        lower(	नीचला(
+        lstrip(	बाया_छा(
+        maketrans(	अनुवाद_तालिका(
+        partition(	बांटे(
+        replace(	बदलो(
+        rfind(	उ_खोजौ(
+        rindex(	उ_सूचक(
+        rpartition(	उ_विभाजन(
+        rsplit(	बाया_विभाजन(
+        rstrip(	बाया_उघाडो(
+        split(	उघाडो(
+        splitlines(	पंक्ति_उघाडो(
+        startswith(	शूरू_में(
+        swapcase(	अक्षर_बदलो(
+        title(	शीर्षक(
+        translate(	अनुवाद(
+        upper(	ऊपरी(
+        zfill(	शू_भरो(
+        append(	जोडो(
+        clear(	साफ(
+        copy(	नकल(
+        extend(	बढाओ(
+        insert(	डालो(
+        pop(	निकालो(
+        remove(	हटाओ(
+        reverse(	उलटा(
+        sort(	क्रम(
+        fromkeys(	चाबी_से(
+        get(	लो(
+        items(	वस्तूए(
+        keys(	पूंजी(
+        popitem(	वस्तू_निकालो(
+        setdefault(	शेष_रखो(
+        update(	नया_जोडो(
+        values(	मान(
+        add(	जमा(
+        difference(	अंतर(
+        difference_update(	अंतर_निकालो(
+        discard(	रद्द(
+        intersection(	सर्वनिष्ठ(
+        intersection_update(	सर्वनिष्ठ_निकालो(
+        isdisjoint(	भिन्न_है(
+        issubset(	उपसमुच्चय_है(
+        issuperset(	उच्चसमुच्चय_है(
+        symmetric_difference(	सममित_अंतर(
+        symmetric_difference_update(	सममित_अंतर_जोडो(
+        union(	सम्मिलन(
+        update(	सम्मिलन_जोडो(
+        . .
+        == =
+        = <-"""
+    id_pair_arr = id_string.strip().split('\n')
+    ids = []
+    
+    for id_pair in id_pair_arr:
+        ids.append(tuple(id_pair.split()))
     return ids
 
 
