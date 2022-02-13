fe7610adc4a5447ff6ac2b530120b743a4363b21
Atul Snapy <atulyadav7096@gmail.com>
2022-02-13 19:50:49 +0530

    not imp : test file

--- test/_hython_/im.py
+++
@@ -1,9 +0,0 @@
-import os      # line-1
-try:
-    def yay():      # line-2
-        print("YAYYY")      # line-3
-        print(os.getcwd())      # line-4
-        print("YAYY? GAINA")      # line-5
-
-except Exception as e:
-    print(e)
--- test/im.hy
+++
@@ -1,5 +0,0 @@
-आयात os
-def yay():
-    print("YAYYY")
-    लिखो(os.getcwd())
-    print("YAYY? GAINA")
